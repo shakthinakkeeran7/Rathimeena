4690704778f74fa7d7ac30d239f3314e58253c98
unknown <shakthinakkeeran@gmail.com>
2023-02-26 23:46:34 +0530

    first commit

---
+++ src/main/java/co/in/rathimeena/Rathimeena/App.java
@@ -0,0 +1,13 @@
+package co.in.rathimeena.Rathimeena;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
